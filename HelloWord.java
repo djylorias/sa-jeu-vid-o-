3a4deadb43240412e4b44ea3ca882b487d2e9df6
djylorias <118692997+djylorias@users.noreply.github.com>
2022-11-21 13:25:53 +0100

    Create HelloWord.java

---
+++ HelloWord.java
@@ -0,0 +1,16 @@
+class HelloWord extends Program{
+    
+    final int TailleMenu = 20;
+
+    void afficherIdentifieur(){
+        for(int i=0; i<TailleMenu; i++){
+            if(i==0 || i==TailleMenu){
+                print("|")
+            }
+        }
+    }
+
+    void algorithm(){
+        print("")
+    }
+}
