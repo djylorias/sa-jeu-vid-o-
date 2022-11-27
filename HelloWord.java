084b64b60a658064eeeb23116c276dc477628763
djylorias <118692997+djylorias@users.noreply.github.com>
2022-11-27 23:43:37 +0100

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,44 +1,72 @@
 class HelloWord extends Program{
     
-    final int TailleMenu = 20;
+    final int xTailleMenu = 150;
+    final int yTailleMenu = 35;
 
     void initialiserIdentifieur(char[][] identifieur){
-        for(int i=0; i<TailleMenu; i++){
-            for(int j=0; j<TailleMenu; j++){
-                identifieur[i][j]='╳';
+        for(int i=0; i<yTailleMenu; i++){
+            for(int j=0; j<xTailleMenu; j++){
+                identifieur[i][j]=' '; //╳
             }
         }
     }
 
-    void insérerLigneHorizontale(char[][] menu, int x, int y, int longueur, char c){
-        int max = x+longueur;
-        for(int i=x; i<max; i++){
-            menu[y][i] = c;
+    void initialiserCaractèreBordure(char[] caracteres, char c){
+        if(c=='═'){
+            caracteres[0] = '═';
+            caracteres[1] = '║';
+            caracteres[2] = '╔';
+            caracteres[3] = '╗';
+            caracteres[4] = '╚';
+            caracteres[5] = '╝';
+        }
+        else if(c=='━'){
+            caracteres[0] = '━';
+            caracteres[1] = '┃';
+            caracteres[2] = '┏';
+            caracteres[3] = '┓';
+            caracteres[4] = '┗';
+            caracteres[5] = '┛';
+        }
+        else{
+            for(int i=0; i<length(caracteres); i++){
+                caracteres[i] = c;
+            }
+        }
+    }
+
+    void insérerLigneVerticale(char[][] menu, int x, int y, int longueur, char c){
+        int max = longueur-y;
+        for(int i=0; i<max; i++){
+            menu[i+y][x] = c;
         }
     }
 
     void insérerLigneHorizontale(char[][] menu, int x, int y, int longueur, char c){
-        int max = x+longueur;
-        for(int i=x; i<max; i++){
-            menu[i][y] = c;
+        int max = longueur-x;
+        for(int i=0; i<max; i++){
+            menu[y][i+x] = c;
         }
     }
     
-    void insérerBordure(char[][] menu, int debut, int xmax, int ymax, char c){
-        char vertical = '';
-        char coinHautGauche = '';
-        char coinHautDroit = '';
-        char coinBasGauche = '';
-        char coinBasDroit = '';
-        if(c=='═'){
-            vertical = ''
-        }
+    void insérerBordure(char[][] menu, int xmin, int ymin, int xmax, int ymax, char c){
+        char[] caracteres = new char[6];
+        initialiserCaractèreBordure(caracteres, c);
+
+        insérerLigneHorizontale(menu, xmin, ymin, xmax, caracteres[0]);
+        insérerLigneVerticale(menu, xmax, ymin, ymax, caracteres[1]);
+        insérerLigneHorizontale(menu, xmin, ymax, xmax, caracteres[0]);
+        insérerLigneVerticale(menu, xmin, ymin, ymax, caracteres[1]);
+        menu[ymin][xmin] = caracteres[2];
+        menu[ymin][xmax] = caracteres[3];
+        menu[ymax][xmin] = caracteres[4];
+        menu[ymax][xmax] = caracteres[5];
     }
 
     void afficherIdentifieur(char[][] identifieur){
         String menu = "";
-        for(int i=0; i<TailleMenu; i++){
-            for(int j=0; j<TailleMenu; j++){
+        for(int i=0; i<yTailleMenu; i++){
+            for(int j=0; j<xTailleMenu; j++){
                 menu += identifieur[i][j];
             }
             menu+="\n";
@@ -47,11 +75,12 @@ class HelloWord extends Program{
     }
 
     void algorithm(){
-        char[][] identifieur = new char[TailleMenu][TailleMenu];
-        initialiserIdentifieur(identifieur);
-        afficherIdentifieur(identifieur);
-        println("------------------------------------");
-        insérerLigneHorizontale(identifieur, 4, 4, TailleMenu-8, '═');
-        afficherIdentifieur(identifieur);
+        char[][] Identifieur = new char[yTailleMenu][xTailleMenu];
+        initialiserIdentifieur(Identifieur);
+        afficherIdentifieur(Identifieur);
+        println("--------------------------------------------------------------------");
+        insérerBordure(Identifieur, 0, 0, xTailleMenu-1, yTailleMenu-1, '═');
+        insérerBordure(Identifieur, 20, 5, xTailleMenu-21, yTailleMenu-6, '━');
+        afficherIdentifieur(Identifieur);
     }
 }
